9e53a48264a1ca5550f2056b8d715de3c690349f
mousefly2014 <9900851@qq.com>
2015-06-05 09:06:28 +0800

    jjs
    sdfdf

--- test/ViewController.m
+++ test/ViewController.m
@@ -18,6 +18,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)test:(id)sender {
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
